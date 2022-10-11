f7839239cd4d09289bacbc3d6d61a846ba4698a8
Mohammod Ali <mohammodali651@gmail.com>
2022-10-12 01:50:25 +0600

    added the banner text

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -3,10 +3,10 @@ import './Banner.css'
 
 const Banner = () => {
     return (
-        <div style={{ 
-            backgroundImage: `url("https://images.unsplash.com/photo-1516534775068-ba3e7458af70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80")`, height: '600px', backgroundRepeat: 'no-repeat', marginLeft: '50px'
+        <div className='banner' style={{ 
+            backgroundImage: `url("https://images.unsplash.com/photo-1516534775068-ba3e7458af70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80")`,backgroundSize:'cover', height: '600px', backgroundRepeat: 'no-repeat'
           }} >
-            <h1>This is banner section</h1>
+            <h1>Join The Quiz And Test Yourself</h1>
         </div>
     );
 };
